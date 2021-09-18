250c11d6d1a2fd016427debefaa055a88588b992
olafghanizadeh <laffen91@gmail.com>
2021-09-18 12:46:08 +0200

    :q

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from otree.api import *
 from gettext import gettext
 
 # Moved index function to separate file to keep this file cleaner
+
 author = 'Olaf Ghanizadeh'
 doc = """
 A crude and simple implementation of the Holt/Laury(2002) lottery.
@@ -35,28 +36,57 @@ def make_field():
 class Constants(BaseConstants):
     name_in_url = 'risk_lottery'
     players_per_group = None
-    num_rounds = 2
+    num_rounds = 3
     num_choices = 10
     # Defining Lottery Payoffs in a dict
     payoffs = {"A": [20, 16], "B": [38, 1]}
     index = create_index(num_choices)
-
-
-class Subsession(BaseSubsession):
-    """Creating the lottery subsessions"""
-
-
-class Group(BaseGroup):
-    pass
+    treatments = {
+        'lo':
+            {
+                1: {
+                    'multiplier': 1,
+                    'hypothetical': False,
+                    'test': True
+                },
+                2: {
+                    'multiplier': 1,
+                    'hypothetical': False,
+                    'test': False
+                },
+                3: {
+                    'multiplier': 20,
+                    'hypothetical': False,
+                    'test': False
+                }
+            },
+        'hi': {
+            1: {
+                'multiplier': 1,
+                'hypothetical': False,
+                'test': True
+            },
+            2: {
+                'multiplier': 1,
+                'hypothetical': False,
+                'test': False
+            },
+            3: {
+                'multiplier': 90,
+                'hypothetical': False,
+                'test': False
+            }
+        }
+    }
 
 
 class Player(BasePlayer):
     """The oTree class that generates the info PER PLAYER"""
-    treatment = models.StringField()
     # Initiate fields to be populated by app
     choice_to_pay = models.StringField()
     option_chosen = models.IntegerField()
     option_chosen_letter = models.StringField()
+    treatment = models.StringField()
     # Function to set payoffs for each player
 
     n = len(index)
@@ -70,31 +100,44 @@ class Player(BasePlayer):
     del n
 
 
+class Subsession(BaseSubsession):
+    """Creating the lottery subsessions"""
+
+
+class Group(BaseGroup):
+    pass
+
+
 # FUNCTIONS
 def creating_session(subsession: Subsession):
     """Method to initiate a session"""
-    for player in subsession.get_players():
-        player.treatment = random.choice(["A", "B", "C", "D"])
-        print('player assigned treatment:', player.treatment)
+    if subsession.round_number == 1:
+        for player in subsession.get_players():
+            participant = player.participant
+            participant.treatment = random.choice(list(Constants.treatments))
+            print('player assigned treatment:', participant.treatment)
 
     # Set Constant num.choices to n for easier reuse
     n = Constants.num_choices
+
     # Multiplier to test for incentive effects
-    multiplier = subsession.session.config['multiplier']
-    # Multiply payoffs by session.config.multiplier
-    payoffs = {
-        key: [i * multiplier for i in value]
-        for (key, value) in Constants.payoffs.items()
-    }
+    for player in subsession.get_players():
+        multiplier = Constants.treatments[player.participant.treatment][subsession.round_number]['multiplier']
+        print('multiplier: ' + str(multiplier))
+        participant = player.participant
+        participant.payoffs = {
+            key: [i for i in value]
+            for (key, value) in Constants.payoffs.items()
+        }
+
     # Store in session variables
-    subsession.session.vars['payoffs'] = payoffs
     subsession.session.vars['index'] = index
     probs = [i / n for i in index]
     inverse_p = [1 - p for p in probs]
     subsession.session.vars['probs'] = probs
     subsession.session.vars['inverse_probs'] = inverse_p
-    formatted_p = ["{0:.0f}".format(p * 100) + "%" for p in probs]
-    formatted_inverse_p = ["{0:.0f}".format(p * 100) + "%" for p in inverse_p]
+    formatted_p = [f"{int(p * 100)}%" for p in probs]
+    formatted_inverse_p = [f"{int(p * 100)}%" for p in inverse_p]
     form_fields = ['choice_' + str(k) for k in index]
     choices = list(zip(index, form_fields, formatted_p, formatted_inverse_p))
     subsession.session.vars['choices'] = choices
@@ -110,9 +153,7 @@ def creating_session(subsession: Subsession):
 def set_payoffs(player: Player):
     """When 'set_payoffs' is called, the player's payoff is set"""
     # Call the payoff dictionary, which contains updated values with multiplier
-    payoffs = player.session.vars['payoffs']
-    # Call the number of choices to create the probability distribution to draw from
-    n = Constants.num_choices
+    payoffs = player.participant.payoffs
     # get the choice that was randomly drawn in 'creating_session'
     player.choice_to_pay = player.participant.vars['choice_to_pay']
     print(player.choice_to_pay)
@@ -131,8 +172,8 @@ def set_payoffs(player: Player):
     del i
     del j
     # Assign the outcomes to lists
-    a = [cu(payoffs['A'][0]), cu(payoffs['A'][1])]
-    b = [cu(payoffs['B'][0]), cu(payoffs['B'][1])]
+    a = [payoffs['A'][0], payoffs['A'][1]]
+    b = [payoffs['B'][0], payoffs['B'][1]]
     # If the player chose 'A'
     if player.option_chosen == 0:
         # Numpy function that picks an element in a list according to probability distribution, first argument is
@@ -140,30 +181,19 @@ def set_payoffs(player: Player):
         # of probabilities
         drawn = np.random.choice(a, 1, p)
         # Run the draw once, numpy creates a list with 1 element, and we access it by getting the 0-index.
-        player.payoff = drawn[0]
+        player.payoff = drawn[0] * Constants.treatments[player.participant.treatment][player.round_number]['multiplier']
         player.option_chosen_letter = 'A'
     # If the player chose 'B'
     elif player.option_chosen == 1:
         drawn = np.random.choice(b, 1, p)
-        player.payoff = drawn[0]
+        player.payoff = drawn[0] * Constants.treatments[player.participant.treatment][player.round_number]['multiplier']
         player.option_chosen_letter = 'B'
-
-
-# PAGES
-# Expose variables for all templates. Attempted to create this as prettier function, but was not able.
-def shared_vars(self):
-    return {
-        'lottery_a_hi': c(self.session.vars['payoffs']['A'][0]),
-        'lottery_a_lo': c(self.session.vars['payoffs']['A'][1]),
-        'lottery_b_hi': c(self.session.vars['payoffs']['B'][0]),
-        'lottery_b_lo': c(self.session.vars['payoffs']['B'][1]),
-        'num_choices': Constants.num_choices,
-    }
 
 
 # Class for the DecisionPage. Inherits attributes from Page Class
 class DecisionPage(Page):
     form_model = 'player'
+
     # Unzip the list of choices, in order to create form fields corresponding to the number of choices
     @staticmethod
     def get_form_fields(player: Player):
@@ -174,13 +204,21 @@ class DecisionPage(Page):
     # Expose variables that will only be available on this page.
     @staticmethod
     def vars_for_template(player: Player):
+        multiplier = Constants.treatments[player.participant.treatment][player.round_number]['multiplier']
         return {
             "choices": player.session.vars['choices'],
-            'lottery_a_hi': cu(player.session.vars['payoffs']['A'][0]),
-            'lottery_a_lo': cu(player.session.vars['payoffs']['A'][1]),
-            'lottery_b_hi': cu(player.session.vars['payoffs']['B'][0]),
-            'lottery_b_lo': cu(player.session.vars['payoffs']['B'][1]),
+            'lottery_a_hi': player.participant.payoffs['A'][0] *
+                            multiplier,
+            'lottery_a_lo': player.participant.payoffs['A'][1] *
+                            multiplier,
+            'lottery_b_hi': player.participant.payoffs['B'][0] *
+                            multiplier,
+            'lottery_b_lo': player.participant.payoffs['B'][1] *
+                            multiplier,
             'num_choices': Constants.num_choices,
+            'test': Constants.treatments[player.participant.treatment][player.round_number]['test'],
+            'hypo': Constants.treatments[player.participant.treatment][player.round_number]['hypothetical'],
+            'treatment': next(iter(Constants.treatments[player.participant.treatment]))
         }
 
     # Triggers the function that set draws the payoff of the user before the user is taken to the result page. This
@@ -198,6 +236,12 @@ class ResultsPage(Page):
         return {
             "index_to_pay": player.participant.vars['index_to_pay'],
         }
+
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        if (Constants.treatments[player.participant.treatment][player.round_number][
+            'test']) or Constants.treatments[player.participant.treatment][player.round_number]['hypothetical']:
+            setattr(player, 'payoff', 0)
 
 
 # The sequence the app will order the pages.
